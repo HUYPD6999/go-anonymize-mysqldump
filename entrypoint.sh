7bc40504c08c929e2063e942355962c1fad38aa2
root <root@ip-172-31-36-71.eu-west-1.compute.internal>
2022-04-01 08:11:40 +0000

    update entrypoint with env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo "$s3"
-echo "$FILE_NAME"
+echo "$S3_BUCKET_SOURCE"
+echo "$S3_BUCKET_TARGET"
+echo "$S3_OBJ"
 SUFFIX=".gz"
-FILE_NAME_REMOVE_SUFFIX=${FILE_NAME/%$SUFFIX}
+FILE_NAME_REMOVE_SUFFIX=${S3_OBJ/%$SUFFIX}
 DATE=$(date +"%F-%H")
 echo "===Start mask db==="
-aws s3 cp s3://"$s3"/"$FILE_NAME" .
+aws s3 cp s3://"$S3_BUCKET_SOURCE"/"$S3_OBJ" .
 echo "===Start Gunzip and MaskDb==="
 echo $FILE_NAME_REMOVE_SUFFIX
-gunzip $FILE_NAME
+gunzip $S3_OBJ
 cat $FILE_NAME_REMOVE_SUFFIX | anonymize-mysqldump --config config.json 2> /var/log/errors.log | gzip > $FILE_NAME_REMOVE_SUFFIX-masked-$DATE.sql.gz
+echo "===Finish MaskDB==="
+echo "===Upload File To s3==="
+aws s3 cp $FILE_NAME_REMOVE_SUFFIX-masked-$DATE.sql.gz s3://"$S3_BUCKET_TARGET"/
 echo "===End==="
-aws s3 cp $FILE_NAME_REMOVE_SUFFIX-masked-$DATE.sql.gz s3://"$s3"/
